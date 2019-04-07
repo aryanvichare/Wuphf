60b4be813622869017baf59b1bcb6a2f9116e0fb
Ganesh Susarla <ganesh@Ganeshs-MacBook-Pro.local>
2019-04-07 04:34:21 -0700

    seecond commit

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
 var url = require('../config/keys').MongoURI;
+var async = require('async');
 var router = express.Router();
 
 var User = require('../models/user');
+var buddies;
 
 router.get('/add-friend', (req, res) => {
     res.render('add-friend', {
@@ -12,15 +14,16 @@ router.get('/add-friend', (req, res) => {
 });
 
 router.post('/add-friend', (req, res) => {
+    buddies = []
     MongoClient.connect(url, (err, db) => {
         var dbo = db.db('test');
-        var query = { name: req.body.name };
         dbo.collection('users').find({ name: { $regex: new RegExp('^' + req.body.name, 'i') } }).toArray((err, friends) => {
             if (err) throw err
             var trueFriends = []
             for (var i = 0; i < friends.length; i++) {
                 if (friends[i].name != req.user.name) {
                     trueFriends.push(friends[i])
+                    buddies.push(friends[i])
                 }
             }
             res.render('add-friend', {
@@ -32,10 +35,28 @@ router.post('/add-friend', (req, res) => {
 })
 
 router.post('/add-new-friend', (req, res) => {
+    console.log('body: ' + JSON.stringify(req.body));
     var currFriends = req.user.friends;
-    console.log(req.body.formData.email)
-    currFriends.push(req.body.formData.email)
-    res.redirect('/')
+    if(!currFriends.includes(req.body.friendEmail)){
+    currFriends.push(req.body.friendEmail)
+    
+    
+    MongoClient.connect(url, (err, db) => {
+        var dbo = db.db('test');
+        dbo.collection('users').update({email: req.user.email}, {$set: {friends:currFriends}}, (err, result) => {
+            if(err) throw err
+        })
+
+        dbo.collection('users').findOne({email: req.body.friendEmail}, (err, result) => {
+            var friendArr = result.friends;
+            friendArr.push(req.user.email);
+            dbo.collection('users').update({email: req.body.friendEmail},{$set: {friends:friendArr}}, (err, result) => {
+                if(err) throw err
+            })
+        })
+
+    })
+}
 })
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var url = require('../config/keys').MongoURI;
 var { ensureAuthenticated } = require('../config/auth');
-
 router.get('/', (req, res) => {
     res.render('home-page');
 });
 
 router.get('/dashboard', (req, res) => {
+    var friendObjects = [];
+    MongoClient.connect(url, (err, db) => {
+        var dbo = db.db('test');
+
+        // var col = dbo.collection('users').findOne({})
+        // var arr = Array.from(col)
+        // console.log(col);
+        // arr.forEach(element => {
+        //     if(req.user.friends.includes(element.email)){
+        //         friendObjects.push(element);
+        //     }
+        // });
+        // console.log(friendObjects);
+
+        req.user.friends.forEach(friend => {
+            dbo.collection('users').findOne({ email: friend }, (err, result) => {
+                if(err) throw err
+                var object = result;
+                friendObjects.push(object);
+            })
+        });
+
+            console.log(friendObjects)
+        //console.log(friendObjects);
+
+    })
     res.render('dashboard', {
         name: req.user.name,
         phone: req.user.phone,
-        email: req.user.email
+        email: req.user.email,
+        friends: friendObjects
     });
+
 });
 
 
